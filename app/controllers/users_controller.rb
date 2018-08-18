e8f297c3fcd929d08c4bb2a6b920575d5e7141c7
Shis <wishmasta@gmail.com>
2018-08-18 13:05:33 +0000

    add show and update - working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,68 @@
 class UsersController < ApplicationController
 
-def new
+  def index
+    
+    @users = User.all
+    
+  end
+  
+  def new
+  
+    @user = User.new
+  
+  end
+  
+  def create
+  
+    @user = User.new(user_params)
+  
+  if @user.save
+  
+    flash[:success] = "Welcome to the alpha blog #{@user.username}"
+  
+    redirect_to articles_path
+  
+  else
+  
+    render 'new'
+  
+  end
+  end
 
-@user = User.new
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
 
-end
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+    
+    flash[:success] = "Your account was updated successfully"
+    
+    redirect_to articles_path
+    
+    else
+    
+    render 'edit'
+    
+    end
 
-def create
+  end
 
-@user = User.new(user_params)
 
-if @user.save
+  def show
+    @user = User.find(params[:id])
+  end
 
-flash[:success] = "Welcome to the alpha blog #{@user.username}"
 
-redirect_to articles_path
-
-else
-
-render 'new'
-
-end
-
-end
 
 private
 
-def user_params
+  def user_params
 
-params.require(:user).permit(:username, :email, :password)
+  params.require(:user).permit(:username, :email, :password)
 
-end
-
+  end
 end
--- app/models/concerns/user.rb
+++ app/models/concerns/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+has_many :articles
+
 validates :username, presence: true,
 
 uniqueness: { case_sensitive: false },

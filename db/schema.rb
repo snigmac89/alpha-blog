235e75131e7d321fd97c08fd273ff1d01b1685b3
Shis <wishmasta@gmail.com>
2018-09-15 05:21:01 +0000

    Add association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180826030333) do
+ActiveRecord::Schema.define(version: 20180915050834) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +27,12 @@ ActiveRecord::Schema.define(version: 20180826030333) do
   end
 
   create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "catergories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
